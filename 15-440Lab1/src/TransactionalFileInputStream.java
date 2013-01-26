8d43920da4b8fcb5bb589895b5521e04bdaec055
Ithilien <Ithilien@home>
2013-01-26 16:39:03 -0500

    More Work On IO

--- 15-440Lab1/src/TransactionalFileInputStream.java
+++ 15-440Lab1/src/TransactionalFileInputStream.java
@@ -9,9 +9,11 @@ public class TransactionalFileInputStream extends FileInputStream implements Ser
 	private static final long serialVersionUID = -7459982254823335052L;
 	private int filePointer;
 	
-	public TransactionalFileInputStream(File arg0) throws FileNotFoundException {
+	public TransactionalFileInputStream(String arg0)
+			throws FileNotFoundException {
 		super(arg0);
 		this.filePointer = 0;
 	}
-
+	
+	
 }
--- 15-440Lab1/src/TransactionalFileOutputStream.java
+++ 15-440Lab1/src/TransactionalFileOutputStream.java
@@ -1,19 +1,18 @@
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
 
 
-public class TransactionalFileOutputStream extends FileInputStream implements Serializable{
+public class TransactionalFileOutputStream extends FileOutputStream implements Serializable{
 
 	private static final long serialVersionUID = 2488121625537896609L;
 	private int filePointer;
 	
-	public TransactionalFileOutputStream(File file)
+	public TransactionalFileOutputStream(String arg0, boolean arg1)
 			throws FileNotFoundException {
-		super(file);
+		super(arg0, arg1);
 		this.filePointer = 0;
 	}
 	
-
 }
