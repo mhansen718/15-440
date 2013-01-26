68b24d76403ac7be22a76fbae2910be5fb96f74b
Ithilien <Ithilien@home>
2013-01-26 18:24:42 -0500

    Not 100% sure, but these SHOULD make io work.....

--- 15-440Lab1/src/TransactionalFileInputStream.java
+++ 15-440Lab1/src/TransactionalFileInputStream.java
@@ -1,18 +1,46 @@
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 
 
-public class TransactionalFileInputStream extends FileInputStream implements Serializable{
+public class TransactionalFileInputStream extends InputStream implements Serializable {
 
 	private static final long serialVersionUID = -7459982254823335052L;
-	private int filePointer;
+	private long filePointer;
+	private String fileName;
 	
-	public TransactionalFileInputStream(String arg0)
-			throws FileNotFoundException {
-		super(arg0);
+	public TransactionalFileInputStream(String fileName) {
+		super();
 		this.filePointer = 0;
+		this.fileName = fileName;
+	}
+
+	@Override
+	public int read() throws IOException {
+		int byteRead;
+		
+		/* First, open a file for reading
+		 * Then, seek to filePointer and read a byte
+		 * Finally,  update filePointer and close the file*/
+		try {
+			RandomAccessFile readMe = new RandomAccessFile(this.fileName, "r");
+			
+			readMe.seek(this.filePointer);
+			
+			byteRead = readMe.read();
+			
+			this.filePointer = readMe.getFilePointer();
+			
+			readMe.close();
+		}
+		catch (FileNotFoundException excp) {
+			System.out.print("File Not Found: " + this.fileName);
+			byteRead = -1;
+		}
+		
+		return byteRead;
 	}
 	
 	
--- 15-440Lab1/src/TransactionalFileOutputStream.java
+++ 15-440Lab1/src/TransactionalFileOutputStream.java
@@ -1,18 +1,55 @@
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 
 
-public class TransactionalFileOutputStream extends FileOutputStream implements Serializable{
+public class TransactionalFileOutputStream extends OutputStream implements Serializable{
 
 	private static final long serialVersionUID = 2488121625537896609L;
-	private int filePointer;
+	private long filePointer;
+	private String fileName;
 	
-	public TransactionalFileOutputStream(String arg0, boolean arg1)
-			throws FileNotFoundException {
-		super(arg0, arg1);
-		this.filePointer = 0;
+	public TransactionalFileOutputStream(String fileName, boolean append) {
+		super();
+		this.fileName = fileName;
+		if (append) {
+			try {
+				this.filePointer = (new File(this.fileName)).length();
+			}
+			catch (Exception excp) {
+				System.out.print("Error Creating TransactionalFileOutputStream: " + excp)
+			}
+		}
+		else {
+			this.filePointer = 0;
+		}
 	}
+	
+	@Override
+	public void write(int byteWriten) throws IOException {
+		
+		/* First, open a file for writing
+		 * Then, seek to filePointer and write the byte
+		 * Finally,  update filePointer and close the file*/
+		try {
+			RandomAccessFile writeMe = new RandomAccessFile(this.fileName, "rw");
+			
+			writeMe.seek(this.filePointer);
+			
+			writeMe.write(byteWriten);
+			
+			this.filePointer = writeMe.getFilePointer();
+			
+			writeMe.close();
+		}
+		catch (FileNotFoundException excp) {
+			System.out.print("File Not Found: " + this.fileName);
+		}
+	}
+	
+	
 	
 }
