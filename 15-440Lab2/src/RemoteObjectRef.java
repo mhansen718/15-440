bff87fff5d673fc2bb6d627469321446a9650b94
Ithilien <mrrosen@andrew.cmu.edu>
2013-03-02 14:42:23 -0500

    Add constructor

--- 15-440Lab2/src/RemoteObjectRef.java
+++ 15-440Lab2/src/RemoteObjectRef.java
@@ -6,6 +6,14 @@ public class RemoteObjectRef {
 	private String objName;
 	private String objInterface;
 	
+	public RemoteObjectRef(String host, int port, String name, String interfaceName) {
+		super();
+		this.objHost = host;
+		this.objPort = port;
+		this.objName = name;
+		this.objInterface = interfaceName;
+	}
+	
 	public Object localise() {
 		/* TODO: Make this work, for now just stfu eclipse */
 		Object o = new Object();
