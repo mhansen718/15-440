29415e318b0f442beaab034a89a028b659c59589
Ithilien <mrrosen@andrew.cmu.edu>
2013-03-02 21:01:30 -0500

    more done

--- 15-440Lab2/src/RMIProxyHandler.java
+++ 15-440Lab2/src/RMIProxyHandler.java
@@ -18,9 +18,23 @@ public class RMIProxyHandler implements InvocationHandler {
 			throws Throwable { 
 		/* TODO: Package up method, name and args into RMIMessage and send them to host:port
 		 * Then, wait for response and on reponse, either localise remote ref or give out the object itself */
-
-		Object o = new Object();
-		return o;
+		RMIMessage sent = new RMIMessage();
+		RMIMessage received;
+		Object returnObj;
+		
+		/* Check to see if the received method resulted in an exception. If so, throw. */
+		if (received.exception == null) {
+			throw received.exception;
+		}
+		
+		/* If no exception, localise remote references or just pass the value through */
+		if (Remote440.class.isAssignableFrom(received.method.getReturnType())) {
+			returnObj = ((RemoteObjectRef) received.returnValue).localise();
+		} else {
+			returnObj = received.returnValue;
+		}
+		
+		return returnObj;
 	}
 
 }
