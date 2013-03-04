3ac24e5b90e6ee1e11dec065448abd5375e55f22
Ithilien <mrrosen@andrew.cmu.edu>
2013-03-04 15:28:02 -0500

    stuff test

---
+++ 15-440Lab2/src/AdvancedTests.java
@@ -0,0 +1,4 @@
+
+public interface AdvancedTests extends Remote440 {
+
+}
--- 15-440Lab2/src/TestRMIServer.java
+++ 15-440Lab2/src/TestRMIServer.java
@@ -1,11 +1,21 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class TestRMIServer {
 
 	public void main(String[] args) {
 		/* A host for the objects TestRMIClient will call on */
-		BasicTests basic = new BasicTests(10, "HI");
-		AdvancedTests adv = new AdvancedTests();
-		
+		BasicTests basic;
+		AdvancedTests adv;
+		RMIRegistryClient myRMI = new RMIRegistryClient(args[0], Integer.parseInt(args[1]));
+		try {
+			RemoteObjectRef basicRef = new RemoteObjectRef(myRMI.getMyHost(), myRMI.getMyPort(), "basics", "BasicTests", myRMI);
+			RemoteObjectRef advRef = new RemoteObjectRef();
+		} catch (Exception e) {
+			System.out.println("Cannot create RemoteObjectRefs");
+			return;
+		}
 		
 		
 	}
