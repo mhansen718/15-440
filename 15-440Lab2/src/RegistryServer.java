521aeedef35c589ad14bcffce99e554146ef6dca
Ithilien <mrrosen@andrew.cmu.edu>
2013-03-04 20:11:08 -0500

    wrap the wrapper in wrapping paper

---
+++ 15-440Lab2/src/RegistryServer.java
@@ -0,0 +1,10 @@
+
+public class RegistryServer {
+
+	/* Wrapper for the RMIRegistry Server */
+	public static void main(String[] args) {
+		RMIRegistryServer server = new RMIRegistryServer();
+		
+		server.main(args);
+	}
+}
