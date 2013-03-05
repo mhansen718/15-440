19ded3ba69116ec49dbbee69f8c1a9701fcdf280
Ithilien <mrrosen@andrew.cmu.edu>
2013-03-04 20:20:28 -0500

    better debug

--- 15-440Lab2/src/TestRMIServer.java
+++ 15-440Lab2/src/TestRMIServer.java
@@ -9,10 +9,15 @@ public class TestRMIServer {
 		try {
 			myRMI = new RMIRegistryClient(args[0], Integer.parseInt(args[1]));
 			
+			System.out.println("Made new local RMI, now binding");
 			myRMI.bind("basics", adv);
+			System.out.println("Bound 1!");
 			myRMI.bind("adv", adv);
+			System.out.println("Bound 2!");
 			myRMI.rebind("basics", basic);
+			System.out.println("All bound!");
 			
+			System.out.println("Listing Bonds:");
 			for (String obj : myRMI.list()) {
 				System.out.println(obj);
 			}
