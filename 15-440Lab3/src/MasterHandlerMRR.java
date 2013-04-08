5b4347974373f8de9217adb83cb75797db5ac5c4
Ithilien <mrrosen@andrew.cmu.edu>
2013-04-08 18:53:37 -0400

    we should have different threads handle the incoming requests to master, so i madew a class to do that

---
+++ 15-440Lab3/src/MasterHandlerMRR.java
@@ -0,0 +1,17 @@
+
+public class MasterHandlerMRR implements Runnable {
+
+	MasterMRR master;
+	
+	public MasterHandlerMRR(MasterMRR master) {
+		super();
+		this.master = master;
+	}
+	
+	@Override
+	public void run() {
+		/* TODO Add the request to the queue and stuff */
+	}
+
+	
+}
