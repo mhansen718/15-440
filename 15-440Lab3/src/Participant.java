585dde8e5ca24d1f9a047ea9115522200eb283ae
Michael Hansen <mhansen718@gmail.com>
2013-04-07 20:59:33 -0400

    Participant can connect to master now

--- 15-440Lab3/src/Participant.java
+++ 15-440Lab3/src/Participant.java
@@ -1,9 +1,33 @@
 public class Participant {
     
     private int processors;
+    private String host;
+    private int port;
+    Socket socket;
+    ObjectInputStream in;
+    ObjectOutputStream out;
     
     public void main(String args[]) {
-        processors = Runtime.availableProcessors();
+        this.processors = Runtime.availableProcessors();
+        
+        this.host = args[0];
+        
+        try {
+            this.port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            // Our script somehow failed to give a valid integer port... What!?
+            System.exit(-1);
+        }
+        
+        try {
+            this.socket = new Socket(this.host,this.port);
+            this.out = new ObjectOutputStream(this.socket.getOutputStream());
+            this.in = new ObjectInputStream(this.socket.getInputStream());
+        } catch (IOException e) {
+            // Couldn't connect to master, no error because this process was created remotely
+            System.exit(-1);
+        }
+        
         
     }
     
