74355519af723792442609f858ff9e9efb828fe2
Ithilien <mrrosen@andrew.cmu.edu>
2013-04-08 18:22:40 -0400

    there we go

--- 15-440Lab3/src/ParticipantMRR.java
+++ 15-440Lab3/src/ParticipantMRR.java
@@ -3,7 +3,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Participant {
+public class ParticipantMRR {
     
     private int processors;
     private String host;
