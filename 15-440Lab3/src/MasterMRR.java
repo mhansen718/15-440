7e70511584433fd6c877d5038a6a8de81315f33e
Ithilien <mrrosen@andrew.cmu.edu>
2013-04-08 18:33:02 -0400

    start to write master

--- 15-440Lab3/src/MasterMRR.java
+++ 15-440Lab3/src/MasterMRR.java
@@ -1,4 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 
 public class MasterMRR {
 
+	public static void main(String args[]) {
+		RandomAccessFile config = null;
+		String configLineRead = null;
+		
+		/* Check arguments and print usage if incorrect */
+		if (args.length != 1) {
+			System.out.println("java MasterMRR [configuration file path]");
+		}
+		
+		/* Begin by reading in the config file */
+		try {
+			config = new RandomAccessFile(args[0], "r");
+		} catch (FileNotFoundException excpt) {
+			System.out.println(" MasterMRR: Failed to open config file, please check file path and try again");
+			System.exit(-1);
+		}
+		
+		/* Read the lines of the file and parse the information */
+		do {
+			try {
+				configLineRead = config.readLine();
+			} catch (IOException e) {
+				System.out.println(" MasterMRR: Failed to read config file");
+				System.exit(-2);
+			}
+			
+		} while (configLineRead != null);
+	}
 }
