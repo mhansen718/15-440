a3b9e7cfa492bbbec4e4859ddc96a297175b8170
Ithilien <mrrosen@andrew.cmu.edu>
2013-04-11 21:38:29 -0400

    more stuff

--- 15-440Lab3/src/ParticipantStatus.java
+++ 15-440Lab3/src/ParticipantStatus.java
@@ -1,7 +1,11 @@
 import java.io.Serializable;
+import java.util.HashSet;
 
 
 public class ParticipantStatus implements Serializable {
 
-	public
+	private static final long serialVersionUID = -4166192444763637179L;
+
+	public HashSet<Integer> completedTasks;
+	public int power;
 }
--- 15-440Lab3/src/Peon.java
+++ 15-440Lab3/src/Peon.java
@@ -1,11 +1,13 @@
+import java.util.HashMap;
+
 
 public class Peon {
 
 	/* Stores information on the current participants */
-	public Process process;
 	public String host;
 	public int port;
 	public int power;
 	public boolean isDead;
+	public HashMap<Integer, TaskEntry> tasks;
 
 }
