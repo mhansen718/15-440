877780c6258a7b182d00eb12ecc5542e8018e03e
Michael Hansen <mhansen718@gmail.com>
2013-04-13 01:24:01 -0400

    Map should work on a worker thread level now

--- 15-440Lab3/src/ParticipantMinionMRR.java
+++ 15-440Lab3/src/ParticipantMinionMRR.java
@@ -19,6 +19,9 @@ public class ParticipantMinionMRR implements Runnable {
         FileInputStream in;
         ObjectOutputStream out;
         byte[] input;
+        TreeMap<?,ArrayList<?>> redIn1;
+        TreeMap<?,ArrayList<?>> redIn2;
+        ArrayList<?> vals;
 		
 		/* Main loop */
 		while (true) {
@@ -37,7 +40,6 @@ public class ParticipantMinionMRR implements Runnable {
 			
             config = currentTask.config;
             
-            
             // Do we have to map, or is this just a reduce?
             if (currentTask.file2 != null) {
                 nextRecord = currentTask.id.start;
@@ -46,13 +48,24 @@ public class ParticipantMinionMRR implements Runnable {
                     raf = new RandomAccessFile(currentTask.file1, "r");
                     raf.seek(nextRecord * currentTask.recordSize);
                     in = new FileInputStream(raf.getFD);
-                    in.read(input);
-                    mapOut = config.map(readRecord(input));
-                    
+                    redIn1 = new TreeMap();
+                    for (int i = nextRecord; i < currentTask.id.end; i++) {
+                        in.read(input);
+                        mapOut = config.map(readRecord(input));
+                        vals = redIn1.get(mapOut.key);
+                        if (vals == null) {
+                            vals = new ArrayList();
+                            vals.add(mapOut.value);
+                            redIn1.put(mapOut.key,vals);
+                        } else {
+                            vals.add(mapOut.value);
+                        }
+                    }
                 } catch (Exception e) {
                                                                             //TODO: error handling
                 }
 			}
+            
 			/* Add the job to the completed work list */
 			master.completeTask(currentTask.id);
 		}
