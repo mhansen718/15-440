03a34439aab9acdfb175bb9026691ec07974da7a
Ithilien <mrrosen@andrew.cmu.edu>
2013-04-29 19:07:54 -0400

    Start lab4, im doing the simple data points thing

---
+++ 15-440Lab4/src/DataPoints.java
@@ -0,0 +1,11 @@
+
+public class DataPoints {
+
+	public static void main (String[] args) {
+		
+		if (args.length != 3) {
+			System.out.println("usage: java DataPoint [coord file] [# clusters] [# of points]");
+		}
+		
+	}
+}
