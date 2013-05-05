8f075295d2d5b72df2f506dc2e27cc489c8e9bde
Michael Hansen <mhansen718@gmail.com>
2013-05-04 21:36:29 -0400

    Fixed CentroidDNA to use arrays

--- 15-440Lab4/src/CentroidDNA.java
+++ 15-440Lab4/src/CentroidDNA.java
@@ -1,28 +1,26 @@
 import java.io.Serializable;
-import java.util.ArrayList;
-
-
 
 public class CentroidDNA implements Serializable {
 
 	private static final long serialVersionUID = -6603084775267021628L;
 	
-    private ArrayList<ArrayList<Double>> strand;
-    private ArrayList<ArrayList<Integer>> newStrand;
+    private double[][] strand;
+    private int[][] newStrand;
 
 	public CentroidDNA(String DNA) {
         int base;
         
-        this.strand = new ArrayList<ArrayList<Double>>();
+        this.strand = new double[DNA.length()][4];
+        this.newStrand = new int[DNA.length()][4];
         
         for (int i = 0; i < DNA.length(); i++) {
-            this.strand.add(new ArrayList<Double>());
+            this.newStrand[i] = {0,0,0,0};
             base = "ACGT".indexOf(DNA.charAt(i));
             for (int j = 0; j < 4; j++) {
                 if (j == base) {
-                    this.strand[i].add(1);
+                    this.strand[i][j] = 1;
                 } else {
-                    this.strand[i].add(0);
+                    this.strand[i][j] = 0;
                 }
             }
         }
@@ -46,20 +44,19 @@ public class CentroidDNA implements Serializable {
     
     public boolean remean() {
         int dist = 0;
-        ArrayList<ArrayList<int>> temp;
+        int[][] temp;
         for (int i = 0; i < this.strand.length; i++) {
             for (int j = 0; j < 4; j++) {
                 dist += Math.abs(this.strand[i][j] - this.newStrand[i][j]);
             }
         }
         temp = this.newStrand.clone();
-        this.newStrand = new ArrayList<ArrayList<int>>();
+        this.newStrand = new int[this.newStrand.length][4];
         double total;
         for (i = 0; i < this.strand.length; i++) {
-            this.newStrand.add(new ArrayList<int>());
+            this.newStrand[i] = {0,0,0,0};
             total = (double) sum(temp[i]);
             for (j = 0; j < 4; j++) {
-                this.newStrand[i].add(0);
                 this.strand[i][j] = temp[i][j] / total;
             }
         }
@@ -69,12 +66,12 @@ public class CentroidDNA implements Serializable {
     public String output() {
         String DNA = "";
         for (int i = 0; i < this.strand.length; i++) {
-            DNA += "ACGT".charAt(maxIndex(this.strand[i]));
+            DNA += "ACGT".charAt(max(this.strand[i]));
         }
         return DNA;
     }
     
-    private int max(ArrayList<double> list) {
+    private int max(double[] list) {
         int maxIndex = 0;
         for (int i = 1; i < list.length; i++) {
             if (list[i] > list[maxIndex]) {
@@ -84,7 +81,7 @@ public class CentroidDNA implements Serializable {
         return maxIndex;
     }
     
-    private int sum(ArrayList<int> list) {
+    private int sum(int[] list) {
         int total = 0;
         for (int val : list) {
             total += val;
